30b932d15728b052e9b1a7d8fc36cf7cfa1b3582
hudiM <deadcsavo@gmail.com>
2018-12-11 15:45:05 +0100

    plan, and test css

--- data_manager.py
+++ data_manager.py
@@ -26,20 +26,58 @@ from datetime import datetime
 @connection.connection_handler
 def get_all_questions(cursor):
     cursor.execute('SELECT * FROM question;')
-    questions = cursor.fetchall()
-    return questions
+    return cursor.fetchall()
+
+@connection.connection_handler
+def get_five_questions(cursor):
+    cursor.execute('SELECT * FROM question ORDER BY submission_time LIMIT 5;')
+    return cursor.fetchall()
 
 @connection.connection_handler
 def get_question_by_id(cursor, question_id):
     cursor.execute("SELECT * FROM question WHERE ID='{0}';".format(question_id))
-    question = cursor.fetchall()
-    return question[0]
+    return cursor.fetchall()[0]
+
+@connection.connection_handler
+def get_answer(cursor, answer_id):
+    cursor.execute("SELECT message, image, question_id FROM answer WHERE id={0};".format(answer_id))
+    return cursor.fetchone()
 
 @connection.connection_handler
 def get_answers_by_question_id(cursor, question_id):
     cursor.execute("SELECT * FROM answer WHERE question_id='{0}' ORDER BY vote_number DESC, submission_time DESC;".format(question_id))
-    question = cursor.fetchall()
-    return question
+    return cursor.fetchall()
+
+@connection.connection_handler
+def get_comments(cursor, mode, data_id):
+    if mode == 'question':
+        cursor.execute("SELECT * FROM comment WHERE question_id={0};".format(data_id))
+    if mode == 'answer':
+        cursor.execute("SELECT * FROM comment WHERE answer_id={0};".format(data_id))
+    return cursor.fetchall()
+
+@connection.connection_handler
+def get_comment_by_id(cursor, comment_id):
+    cursor.execute("SELECT * FROM comment WHERE id={0};".format(comment_id))
+    return cursor.fetchone()
+
+@connection.connection_handler
+def get_latest_question_id(cursor):
+    cursor.execute('SELECT id FROM question ORDER BY submission_time DESC LIMIT 1;')
+    return cursor.fetchone()['id']
+
+@connection.connection_handler
+def get_question_id_by_answer_id(cursor, answer_id):
+    cursor.execute("SELECT question_id FROM answer WHERE id='{0}';".format(answer_id))
+    return cursor.fetchone()['question_id']
+
+@connection.connection_handler
+def get_question_id_by_comment_id(cursor, comment_id):
+    cursor.execute("SELECT question_id, answer_id FROM comment WHERE id={0}".format(comment_id))
+    ids = cursor.fetchone()
+    if ids['question_id'] is None:
+        return get_question_id_by_answer_id(ids['answer_id'])
+    return ids['question_id']
 
 # ----------------------------------------------------------
 #                   add
@@ -52,12 +90,18 @@ def new_answer(cursor, form, question_id):
 
 @connection.connection_handler
 def new_question(cursor, form):
-    sql_string = "INSERT INTO question (submission_time, view_number, vote_number, title, message, image) " \
-                 "VALUES ('{0}','0','0','{1}','{2}','{3}')".format(datetime.now(),form['title'],form['message'],form['image'])
-    cursor.execute(sql_string)
-    cursor.execute('SELECT id FROM question ORDER BY submission_time DESC LIMIT 1')
-    question_id = cursor.fetchone()
-    return question_id
+    cursor.execute("INSERT INTO question (submission_time, view_number, vote_number, title, message, image) VALUES ('{0}','0','0','{1}','{2}','{3}');".format(datetime.now(),form['title'],form['message'],form['image']))
+    return get_latest_question_id()
+
+@connection.connection_handler
+def new_comment(cursor, mode, form, data_id):
+    if mode == 'question':
+        cursor.execute("INSERT INTO comment (question_id, answer_id, message, submission_time, edited_count) VALUES ('{0}',{1},'{2}','{3}',{4});".format(
+            data_id, 'NULL', form['message'], str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), 'NULL'))
+    if mode == 'answer':
+        cursor.execute("INSERT INTO comment (question_id, answer_id, message, submission_time, edited_count) VALUES ({0},'{1}','{2}','{3}',{4});".format(
+            'NULL', data_id, form['message'], str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), 'NULL'))
+    return
 
 # ----------------------------------------------------------
 #                   edit
@@ -65,8 +109,21 @@ def new_question(cursor, form):
 
 @connection.connection_handler
 def edit_question(cursor, form, question_id):
-    cursor.execute("UPDATE question SET submission_time='{0}',title='{1}',message='{2}',image='{3}' WHERE id={4};".format(str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), form['title'], form['message'], form['image'], question_id))
+    cursor.execute("UPDATE question SET submission_time='{0}',title='{1}',message='{2}',image='{3}' WHERE id={4};".format(
+        str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), form['title'], form['message'], form['image'], question_id))
     return
+
+@connection.connection_handler
+def edit_answer(cursor, form, answer_id):
+    cursor.execute("UPDATE answer SET submission_time='{0}',message='{1}',image='{2}' WHERE id={3};".format(
+        str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")),form['message'],form['image'],answer_id))
+    return get_question_id_by_answer_id(answer_id)
+
+@connection.connection_handler
+def edit_comment(cursor, form, comment_id):
+    cursor.execute("UPDATE comment SET message='{0}', submission_time='{1}' WHERE id={2};".format(
+        form['message'], str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")),comment_id))
+    return get_question_id_by_comment_id(comment_id)
 
 # ----------------------------------------------------------
 #                   delete
@@ -74,15 +131,22 @@ def edit_question(cursor, form, question_id):
 
 @connection.connection_handler
 def delete_question(cursor, question_id):
-    cursor.execute("DELETE FROM answer WHERE question_id='{0}'; DELETE FROM question WHERE id='{0}';".format(str(question_id)))
+    for answer in get_answers_by_question_id(question_id):
+        cursor.execute('DELETE FROM comment WHERE answer_id={0}'.format(answer['id']))
+    cursor.execute("DELETE FROM comment WHERE question_id={0}; DELETE FROM answer WHERE question_id='{0}'; DELETE FROM question WHERE id='{0}';".format(str(question_id)))
     return
 
 @connection.connection_handler
 def delete_answer(cursor, answer_id):
-    cursor.execute("SELECT question_id FROM answer WHERE id='{0}';".format(str(answer_id)))
-    question_id = cursor.fetchone()
-    cursor.execute("DELETE FROM answer WHERE id='{0}';".format(str(answer_id)))
-    return question_id['question_id']
+    question_id = get_question_id_by_answer_id(answer_id)
+    cursor.execute("DELETE FROM comment WHERE answer_id={0};DELETE FROM answer WHERE id='{0}';".format(str(answer_id)))
+    return question_id
+
+@connection.connection_handler
+def delete_comment(cursor, comment_id):
+    question_id = get_question_id_by_comment_id(comment_id)
+    cursor.execute("DELETE FROM comment WHERE id='{0}';".format(str(comment_id)))
+    return question_id
 
 # ----------------------------------------------------------
 #                   vote
@@ -92,25 +156,24 @@ def delete_answer(cursor, answer_id):
 @connection.connection_handler
 def vote(cursor, mode, direction, data_id):
     if direction == 'up':
-        cursor.execute("UPDATE {0} SET vote_number=vote_number+1 WHERE id={1}".format(mode, data_id))
+        cursor.execute("UPDATE {0} SET vote_number=vote_number+1 WHERE id={1};".format(mode, data_id))
     if direction == 'down':
-        cursor.execute("UPDATE {0} SET vote_number=vote_number-1 WHERE id={1}".format(mode, data_id))
+        cursor.execute("UPDATE {0} SET vote_number=vote_number-1 WHERE id={1};".format(mode, data_id))
     if mode == 'answer':
-        cursor.execute("SELECT question_id FROM answer WHERE id='" + str(data_id) + "';")
-        question_id = cursor.fetchone()['question_id']
+        question_id = get_question_id_by_answer_id(data_id)
         page_view_counter('down', question_id)
         return question_id
     page_view_counter('down', data_id)
     return
 
 # ----------------------------------------------------------
-#                   vote
+#                   views
 # ----------------------------------------------------------
 
 @connection.connection_handler
 def page_view_counter(cursor, mode, question_id):
     if mode == 'up':
-        cursor.execute("UPDATE question SET view_number=view_number+1 WHERE id={0}".format(question_id))
+        cursor.execute("UPDATE question SET view_number=view_number+1 WHERE id={0};".format(question_id))
     if mode == 'down':
-        cursor.execute("UPDATE question SET view_number=view_number-1 WHERE id={0}".format(question_id))
+        cursor.execute("UPDATE question SET view_number=view_number-1 WHERE id={0};".format(question_id))
     return
--- server.py
+++ server.py
@@ -10,18 +10,21 @@ app = Flask(__name__)
 # ------------------------
 
 @app.route('/')
-@app.route('/list')
 def route_index():
-    questions = data_manager.get_all_questions()
+    questions = data_manager.get_five_questions()
     questions = sorted(questions,key=lambda k: k['submission_time'], reverse=True)
-    return render_template('list.html', questions = questions)
+    return render_template('list.html', questions = questions, index=True)
 
+@app.route('/list')
+def route_list():
+    questions = data_manager.get_all_questions()
+    return render_template('list.html', questions = questions)
 
 @app.route('/add-question', methods=['GET', 'POST'])
 def route_add_question():
     if request.method == 'POST':
         question_id = data_manager.new_question(request.form)
-        return redirect('/question/' + str(question_id['id']))
+        return redirect('/question/' + str(question_id))
     return render_template('new_question.html', question = None)
 
 
@@ -30,7 +33,11 @@ def route_question(question_id):
     question = data_manager.get_question_by_id(question_id)
     data_manager.page_view_counter('up', question_id)
     answers = data_manager.get_answers_by_question_id(question_id)
-    return render_template('question.html', question=question, answers = answers)
+    question_comments = data_manager.get_comments('question',question_id)
+    answer_comments = []
+    for answer in answers:
+        answer_comments.append(data_manager.get_comments('answer', answer['id']))
+    return render_template('question.html', question=question, answers = answers, question_comments=question_comments, answer_comments=answer_comments)
 
 
 @app.route('/question/<question_id>/delete')
@@ -44,6 +51,19 @@ def route_answer_delete(answer_id):
     question_id = data_manager.delete_answer(answer_id)
     return redirect('/question/'+ str(question_id))
 
+@app.route('/comment/<comment_id>/delete')
+def route_comment_delete(comment_id):
+    question_id = data_manager.delete_comment(comment_id)
+    return redirect('/question/'+ str(question_id))
+
+@app.route('/answer/<answer_id>/edit', methods=['GET', 'POST'])
+def route_answer_edit(answer_id):
+    if request.method == 'POST':
+        question_id = data_manager.edit_answer(request.form, answer_id)
+        return redirect('/question/'+str(question_id))
+    answer = data_manager.get_answer(answer_id)
+    return render_template('new_answer.html',answer=answer)
+
 
 @app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
 def route_question_edit(question_id):
@@ -53,13 +73,34 @@ def route_question_edit(question_id):
     question = data_manager.get_question_by_id(question_id)
     return render_template('new_question.html', question = question)
 
+@app.route('/comment/<comment_id>/edit', methods=['GET', 'POST'])
+def route_comment_edit(comment_id):
+    if request.method == 'POST':
+        question_id = data_manager.edit_comment(request.form, comment_id)
+        return redirect('/question/'+str(question_id))
+    comment = data_manager.get_comment_by_id(comment_id)
+    return render_template('new_comment.html', comment=comment)
 
 @app.route('/question/<question_id>/new-answer', methods=['GET','POST'])
 def route_new_answer(question_id):
     if request.method == 'POST':
         data_manager.new_answer(request.form, question_id)
         return redirect('/question/'+str(question_id))
-    return render_template('new_answer.html', question_id = question_id)
+    return render_template('new_answer.html', question_id = question_id, answer=None)
+
+@app.route('/question/<question_id>/new-comment', methods=['GET','POST'])
+def route_new_comment_question(question_id):
+    if request.method == 'POST':
+        data_manager.new_comment('question', request.form, question_id)
+        return redirect('/question/'+str(question_id))
+    return render_template('new_comment.html', comment=None)
+
+@app.route('/answer/<answer_id>/new-comment', methods=['GET','POST'])
+def route_new_comment_answer(answer_id):
+    if request.method == 'POST':
+        data_manager.new_comment('answer', request.form, answer_id)
+        return redirect('/question/'+str(data_manager.get_question_id_by_answer_id(answer_id)))
+    return render_template('new_comment.html', comment=None)
 
 
 @app.route('/answer/<answer_id>/vote-up')
